99c7f6fd2209fba9053028c41f549888f3503e8d
Colin Snook <cfsnook@users.sourceforge.net>
2010-02-07 23:58:16 +0000

    commented out code that needs EMFT - doesn't seem to be needed and causes problems in installation
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk/EMF_EventB@8200 1434b563-b632-4741-aa49-43a3a8374d2e

--- org.eventb.emf.structuredEditor/src/org/eventb/emf/edit/propertySections/abstracts/AbstractEventBPropertySection.java
+++ org.eventb.emf.structuredEditor/src/org/eventb/emf/edit/propertySections/abstracts/AbstractEventBPropertySection.java
@@ -16,8 +16,6 @@ import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.ui.provider.PropertySource;
-import org.eclipse.emf.transaction.TransactionalEditingDomain;
-import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.widgets.Composite;
@@ -31,15 +29,17 @@ import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 import org.eventb.emf.core.EventBElement;
 import org.eventb.emf.structuredEditor.EventBStructuredEditor;
 
-
 /**
- * Abstract basis for custom property sheets that show model properties
- * (as opposed to the model property tab which shows a more EMF style list of model properties)
- *
- * This property sheet is intended to work on a single selected model element. If more are selected only one will be used.
- *
- * This code was originally based on the hockey league example available from the eclipse website
- *
+ * Abstract basis for custom property sheets that show model properties (as
+ * opposed to the model property tab which shows a more EMF style list of model
+ * properties)
+ * 
+ * This property sheet is intended to work on a single selected model element.
+ * If more are selected only one will be used.
+ * 
+ * This code was originally based on the hockey league example available from
+ * the eclipse website
+ * 
  * @author cfs
  */
 
@@ -54,45 +54,48 @@ public abstract class AbstractEventBPropertySection extends AbstractPropertySect
 	 * the GMF editing domain for the current diagram editor
 	 */
 	protected EditingDomain editingDomain = null;
-	public EditingDomain getEditingDomain() { return editingDomain;}
+
+	public EditingDomain getEditingDomain() {
+		return editingDomain;
+	}
 
 	/**
-	 * The current selected UMLB element or the first object in the selection when
-	 * multiple objects are selected.
+	 * The current selected UMLB element or the first object in the selection
+	 * when multiple objects are selected.
 	 */
 	protected EventBElement owner;
-
 
 	/**
 	 *
 	 */
 	protected Composite parent;
 
-	/* Doesn't work because don't have the labels so disabled
-	 * Get the standard label width when labels for sections line up on the left
-	 * hand side of the composite. We line up to a fixed position, but if a
-	 * string is wider than the fixed position, then we use that widest string.
-	 *
-	 * @param parent
-	 *            The parent composite used to create a GC.
-	 * @param labels
-	 *            The list of labels.
+	/*
+	 * Doesn't work because don't have the labels so disabled Get the standard
+	 * label width when labels for sections line up on the left hand side of the
+	 * composite. We line up to a fixed position, but if a string is wider than
+	 * the fixed position, then we use that widest string.
+	 * 
+	 * @param parent The parent composite used to create a GC.
+	 * 
+	 * @param labels The list of labels.
+	 * 
 	 * @return the standard label width.
 	 */
 	protected int getStandardLabelWidth(final Composite parent, final String[] labels) {
 		return 100;
 
-//		int standardLabelWidth = STANDARD_LABEL_WIDTH;
-//		GC gc = new GC(parent);
-//		int indent = gc.textExtent("XXX").x; //$NON-NLS-1$
-//		for (String element : labels) {
-//			int width = gc.textExtent(element).x;
-//			if (width + indent > standardLabelWidth) standardLabelWidth = width + indent;
-//		}
-//		gc.dispose();
-//		return standardLabelWidth;
+		// int standardLabelWidth = STANDARD_LABEL_WIDTH;
+		// GC gc = new GC(parent);
+		//		int indent = gc.textExtent("XXX").x; //$NON-NLS-1$
+		// for (String element : labels) {
+		// int width = gc.textExtent(element).x;
+		// if (width + indent > standardLabelWidth) standardLabelWidth = width +
+		// indent;
+		// }
+		// gc.dispose();
+		// return standardLabelWidth;
 	}
-
 
 	@Override
 	public void createControls(final Composite parent, final TabbedPropertySheetPage propertySheetPage) {
@@ -100,23 +103,24 @@ public abstract class AbstractEventBPropertySection extends AbstractPropertySect
 		this.parent = parent;
 		this.propertySheetPage = propertySheetPage;
 		IEditorPart activeEditor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-		if (activeEditor instanceof EventBStructuredEditor){
-			editingDomain=((EventBStructuredEditor)activeEditor).getEditingDomain();
+		if (activeEditor instanceof EventBStructuredEditor) {
+			editingDomain = ((EventBStructuredEditor) activeEditor).getEditingDomain();
 		}
 	}
 
-
 	@Override
 	public void setInput(final IWorkbenchPart part, final ISelection selection) {
-		owner=null;
+		owner = null;
 		if (selection.isEmpty() || false == selection instanceof StructuredSelection) {
 			super.setInput(part, selection);
 			return;
 		}
 		final StructuredSelection structuredSelection = (StructuredSelection) selection;
 		ArrayList<Object> transformedSelection = new ArrayList<Object>(structuredSelection.size());
-		for (Object object : structuredSelection.toList()){
-			if (owner==null && object instanceof EventBElement) owner=(EventBElement)object;
+		for (Object object : structuredSelection.toList()) {
+			if (owner == null && object instanceof EventBElement) {
+				owner = (EventBElement) object;
+			}
 			if (object != null) {
 				transformedSelection.add(object);
 			}
@@ -124,19 +128,28 @@ public abstract class AbstractEventBPropertySection extends AbstractPropertySect
 		super.setInput(part, new StructuredSelection(transformedSelection));
 	}
 
-	/* property source provider implementation (copied from generated DiagramPropertySection)*/
+	/*
+	 * property source provider implementation (copied from generated
+	 * DiagramPropertySection)
+	 */
 
 	/**
 	 * @generated
 	 */
 	public IPropertySource getPropertySource(final Object object) {
-		if (object instanceof IPropertySource) { return (IPropertySource) object; }
+		if (object instanceof IPropertySource) {
+			return (IPropertySource) object;
+		}
 		AdapterFactory af = getAdapterFactory(object);
 		if (af != null) {
 			IItemPropertySource ips = (IItemPropertySource) af.adapt(object, IItemPropertySource.class);
-			if (ips != null) { return new PropertySource(object, ips); }
+			if (ips != null) {
+				return new PropertySource(object, ips);
+			}
 		}
-		if (object instanceof IAdaptable) { return (IPropertySource) ((IAdaptable) object).getAdapter(IPropertySource.class); }
+		if (object instanceof IAdaptable) {
+			return (IPropertySource) ((IAdaptable) object).getAdapter(IPropertySource.class);
+		}
 		return null;
 	}
 
@@ -151,9 +164,21 @@ public abstract class AbstractEventBPropertySection extends AbstractPropertySect
 	 * @generated
 	 */
 	protected AdapterFactory getAdapterFactory(final Object object) {
-		if (getEditingDomain() instanceof AdapterFactoryEditingDomain) { return ((AdapterFactoryEditingDomain) getEditingDomain()).getAdapterFactory(); }
-		TransactionalEditingDomain editingDomain = TransactionUtil.getEditingDomain(object);
-		if (editingDomain != null) { return ((AdapterFactoryEditingDomain) editingDomain).getAdapterFactory(); }
+		if (getEditingDomain() instanceof AdapterFactoryEditingDomain) {
+			return ((AdapterFactoryEditingDomain) getEditingDomain()).getAdapterFactory();
+		}
+
+		// Not sure where this bit came from but it requires the EMFT
+		// transaction feature
+		// which is not on the Galileo update site causing problems at
+		// installation
+		// As it doesn't appear to be needed have commented for now. cfs 7/2/10
+
+		// TransactionalEditingDomain editingDomain =
+		// TransactionUtil.getEditingDomain(object);
+		// if (editingDomain != null) { return ((AdapterFactoryEditingDomain)
+		// editingDomain).getAdapterFactory(); }
+
 		return null;
 	}
 
