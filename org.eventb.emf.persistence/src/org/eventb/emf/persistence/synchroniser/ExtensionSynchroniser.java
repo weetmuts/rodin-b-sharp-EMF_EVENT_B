cf7bd9d1f3c8a1cbf553e4d9f5961bd33e5afe0f
Colin Snook <cfsnook@users.sourceforge.net>
2010-03-30 08:47:57 +0000

    Cater for meta-class extenders - i.e. at save do nothing but pass on the parent to the next synchroniser level
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk/EMF_EventB@8548 1434b563-b632-4741-aa49-43a3a8374d2e

--- org.eventb.emf.persistence/src/org/eventb/emf/persistence/synchroniser/ExtensionSynchroniser.java
+++ org.eventb.emf.persistence/src/org/eventb/emf/persistence/synchroniser/ExtensionSynchroniser.java
@@ -4,7 +4,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.EAnnotation;
+import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eventb.emf.core.AbstractExtension;
 import org.eventb.emf.core.CoreFactory;
 import org.eventb.emf.core.CorePackage;
 import org.eventb.emf.core.EventBElement;
@@ -13,10 +16,13 @@ import org.rodinp.core.IAttributeType;
 import org.rodinp.core.IInternalElement;
 import org.rodinp.core.IInternalElementType;
 import org.rodinp.core.IRodinElement;
+import org.rodinp.core.IRodinFile;
 import org.rodinp.core.RodinCore;
 import org.rodinp.core.RodinDBException;
 
 public class ExtensionSynchroniser extends AbstractSynchroniser {
+
+	private static final String metaClassExtenderID = "org.eventb.emf.core.extendedMetaClasses";
 
 	private static final Set<IAttributeType> handledAttributes = new HashSet<IAttributeType>();
 	private static IInternalElementType<?> rodinElementType;
@@ -50,12 +56,34 @@ public class ExtensionSynchroniser extends AbstractSynchroniser {
 
 	@Override
 	public IInternalElement save(final EventBElement emfElement, final IRodinElement rodinParent, final IProgressMonitor monitor) throws RodinDBException {
-		String id = ((Extension) emfElement).getExtensionId();
-		if (id == null || "".equals(id))
-			return null; //some extensions are not intended to be saved
-		rodinElementType = RodinCore.getInternalElementType(id);
-		// create Rodin element
-		IInternalElement rodinElement = super.save(emfElement, rodinParent, monitor);
-		return rodinElement;
+		if (isMetaClassExtension((AbstractExtension) emfElement)) {
+			//if this extension just adds to its parents features, we don't need to do anything, just return the same parent
+			return asInternalElement(rodinParent);
+		} else if (emfElement instanceof Extension) {
+			String id = ((Extension) emfElement).getExtensionId();
+			if (id == null || "".equals(id))
+				return null; //some extensions are not intended to be saved
+			rodinElementType = RodinCore.getInternalElementType(id);
+			// create Rodin element
+			IInternalElement rodinElement = super.save(emfElement, rodinParent, monitor);
+			return rodinElement;
+		} else
+			return null;
+	}
+
+	private boolean isMetaClassExtension(final AbstractExtension extension) {
+		EClassifier eClassifier = extension.eClass();
+		EAnnotation extendedMetaClassAnnotation = eClassifier.getEAnnotation(metaClassExtenderID);
+		return extendedMetaClassAnnotation != null;
+	}
+
+	private IInternalElement asInternalElement(IRodinElement rodinElement) {
+		if (rodinElement instanceof IInternalElement) {
+			return (IInternalElement) rodinElement;
+		} else if (rodinElement instanceof IRodinFile) {
+			return ((IRodinFile) rodinElement).getRoot();
+		} else {
+			return null;
+		}
 	}
 }
