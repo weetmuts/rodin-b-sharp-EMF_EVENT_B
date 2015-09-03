32c74365c57dcf649e19b7120ae1695a41312d76
Colin Snook <cfs@ecs.soton.ac.uk>
2015-09-03 13:43:31 +0100

    add AntiSorter (copied from fr.systerel.navigator) to prevent alphabetical sorting of iUML-B contents.

---
+++ EMF_EventB_Extension/ac.soton.eventb.emf.core.extension.navigator/src/ac/soton/eventb/emf/core/extension/navigatore/sorter/AntiSorter.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2013 Systerel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Systerel - initial API and implementation
+ *******************************************************************************/
+package ac.soton.eventb.emf.core.extension.navigatore.sorter;
+
+import java.text.Collator;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerSorter;
+
+/**
+ * A sorter that does not sort at all. The elements are sorted by the CNF
+ * alphabetically by default, which we do not want. This overrides the sorting
+ * and keeps them in the order they appear.
+ * 
+ * For that, we suppose that the sorting algorithm used by the CNF is stable and
+ * just say that all elements are equal.
+ * 
+ * COPIED FROM fr.systerel.internal.explorer.navigator.sorters
+ */
+public class AntiSorter extends ViewerSorter {
+
+	public AntiSorter() {
+		// do nothing
+	}
+
+	public AntiSorter(Collator collator) {
+		super(collator);
+	}
+
+	/**
+	 * All elements are equal, so that they do not get sorted.
+	 */
+	@Override
+	public int compare(Viewer viewer, Object e1, Object e2) {
+		return 0;
+	}
+
+}
